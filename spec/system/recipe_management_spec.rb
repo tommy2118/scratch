55ac39eebe969f6e0dccf4d6ce2cdb89a9cbaf60
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2019-02-13 22:48:02 -0500

    @wip [ci skip]

---
+++ spec/system/recipe_management_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Recipe management", type: :system do
+  it "enables the ability to create a recipe" do
+    visit "/recipes/new"
+
+    fill_in "Title", with: "Scrambled Eggs"
+    fill_in "Prep time", with: 5
+    fill_in "Cook time", with: 30
+    select "Traditional", from: "Classification"
+    select "Stove top", from: "Cooking method"
+    select "Egg", from: "Protein type"
+    expect(page).to have_text("Recipe successfully created.")
+  end
+end
